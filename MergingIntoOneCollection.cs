cfa6d3a9318cc5a18f9215a96e0a92fc2a5ab8d9
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 13:58:41 +0500

    Merging into one collection

---
+++ MergingIntoOneCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MergingIntoOneCollection
+{
+	internal class MergingIntoOneCollection
+	{
+		static void Main(string[] args)
+		{
+			List<string> collections = new List<string>();
+
+			string[] arrayString1 = { "1", "2", "1" };
+			string[] arrayString2 = { "3", "2" };
+
+			FillCollectionUniqueValues(collections, arrayString1);
+			FillCollectionUniqueValues(collections, arrayString2);
+
+			OutputCollection(collections);
+		}
+
+		static void FillCollectionUniqueValues(List<string> collections, string[] arrayString)
+		{
+			for (int i = 0; i < arrayString.Length; i++)
+			{
+				if (collections.Contains(arrayString[i]) == false)
+					collections.Add(arrayString[i]);
+			}
+		}
+
+		static void OutputCollection(List<string> collections)
+		{
+			for (int i = 0; i < collections.Count; i++)
+				Console.Write(collections[i] + " ");
+
+			Console.WriteLine();
+		}
+	}
+}
